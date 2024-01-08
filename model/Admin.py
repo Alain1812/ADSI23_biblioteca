fe3bc79d25f2647531c86d579571e089cb092691
Alain Pedrueza Basabe <104357836+Alain1812@users.noreply.github.com>
2024-01-08 22:53:21 +0100

    Delete model/Admin.py

--- model/Admin.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from .Connection import Connection
-from .tools import hash_password
-from model import User
-
-class Admin(User):
-    def __init__(self, id, username, email):
-        super().__init__(id, username, email)
